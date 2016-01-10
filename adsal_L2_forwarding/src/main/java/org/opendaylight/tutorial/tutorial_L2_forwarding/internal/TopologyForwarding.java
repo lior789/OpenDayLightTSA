788f1db859f551ada05cce25f32a1662bcf9e952
root <lior789@gmail.com>
2016-01-10 16:13:04 -0500

    second commit

--- adsal_L2_forwarding/src/main/java/org/opendaylight/tutorial/tutorial_L2_forwarding/internal/TopologyForwarding.java
+++ adsal_L2_forwarding/src/main/java/org/opendaylight/tutorial/tutorial_L2_forwarding/internal/TopologyForwarding.java
@@ -19,50 +19,63 @@ package org.opendaylight.tutorial.tutorial_L2_forwarding.internal;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
+import org.opendaylight.controller.hosttracker.IfNewHostNotify;
+import org.opendaylight.controller.hosttracker.IfIptoHost;
+import org.opendaylight.controller.hosttracker.hostAware.HostNodeConnector;
 import org.opendaylight.controller.sal.action.Action;
 import org.opendaylight.controller.sal.action.Output;
 import org.opendaylight.controller.sal.core.ConstructionException;
+import org.opendaylight.controller.sal.core.Edge;
 import org.opendaylight.controller.sal.core.Node;
 import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.core.Path;
 import org.opendaylight.controller.sal.flowprogrammer.Flow;
 import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
 import org.opendaylight.controller.sal.match.Match;
-import org.opendaylight.controller.sal.match.MatchField;
 import org.opendaylight.controller.sal.match.MatchType;
-import org.opendaylight.controller.sal.packet.BitBufferHelper;
 import org.opendaylight.controller.sal.packet.Ethernet;
 import org.opendaylight.controller.sal.packet.IDataPacketService;
 import org.opendaylight.controller.sal.packet.IListenDataPacket;
+import org.opendaylight.controller.sal.packet.IPv4;
 import org.opendaylight.controller.sal.packet.Packet;
 import org.opendaylight.controller.sal.packet.PacketResult;
 import org.opendaylight.controller.sal.packet.RawPacket;
+import org.opendaylight.controller.sal.routing.IRouting;
 import org.opendaylight.controller.sal.utils.Status;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
 import org.opendaylight.controller.switchmanager.Switch;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
-import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TutorialL2Forwarding implements IListenDataPacket {
+public class TopologyForwarding implements IListenDataPacket, IfNewHostNotify {
 	private static final Logger logger = LoggerFactory
-			.getLogger(TutorialL2Forwarding.class);
+			.getLogger(TopologyForwarding.class);
 	private ISwitchManager switchManager = null;
 	private IFlowProgrammerService programmer = null;
 	private IDataPacketService dataPacketService = null;
-	private final Map<Node, Map<Long, NodeConnector>> mac_to_port_per_switch = new HashMap<Node, Map<Long, NodeConnector>>();
-	private final String function = "switch";
+	private IfIptoHost hostTracker;
+	private IRouting routing;
+	private HashMap<Flow, Node> _allFlows;
+	private HashMap<HostNodeConnector, List<Flow>> _hostFlows;
 
 	void setDataPacketService(IDataPacketService s) {
 		this.dataPacketService = s;
@@ -96,6 +109,26 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 		}
 	}
 
+	void setHostTracker(IfIptoHost s) {
+		this.hostTracker = s;
+	}
+
+	void setRoutingService(IRouting s) {
+		this.routing = s;
+	}
+
+	void unsetRoutingService(IRouting s) {
+		if (this.routing == s) {
+			this.routing = null;
+		}
+	}
+
+	void unsetHostTracker(IfIptoHost s) {
+		if (this.hostTracker == s) {
+			this.hostTracker = null;
+		}
+	}
+
 	/**
 	 * Function called by the dependency manager when all the required
 	 * dependencies are satisfied
@@ -103,21 +136,6 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 	 */
 	void init() {
 		logger.info("Initialized");
-		// Disabling the SimpleForwarding and ARPHandler bundle to not conflict
-		// with this one
-		BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass())
-				.getBundleContext();
-		for (Bundle bundle : bundleContext.getBundles()) {
-			if (bundle.getSymbolicName().contains("simpleforwarding")) {
-				try {
-					bundle.uninstall();
-				} catch (BundleException e) {
-					logger.error(
-							"Exception in Bundle uninstall "
-									+ bundle.getSymbolicName(), e);
-				}
-			}
-		}
 
 	}
 
@@ -128,6 +146,19 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 	 * 
 	 */
 	void destroy() {
+		logger.info("deleting forwarding flows");
+		for (Flow flow : _allFlows.keySet()) {
+			removeFlow(flow);
+
+		}
+	}
+
+	private void removeFlow(Flow flow) {
+		Status status = programmer.removeFlow(_allFlows.get(flow), flow);
+		if (!status.isSuccess())
+			logger.error("error while removing flow: "
+					+ status.getDescription());
+		_allFlows.remove(flow);
 	}
 
 	/**
@@ -136,6 +167,8 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 	 * 
 	 */
 	void start() {
+		_allFlows = new HashMap<>();
+		_hostFlows = new HashMap<>();
 		if (isRemoveAllFlows()) {
 			logger.info("DELTETING ALL FLOWS!");
 			List<Switch> switches = switchManager.getNetworkDevices();
@@ -179,6 +212,56 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 	 */
 	void stop() {
 		logger.info("Stopped");
+
+	}
+
+	static private Inet4Address intToInetAddress(int i) {
+		byte b[] = new byte[] { (byte) ((i >> 24) & 0xff),
+				(byte) ((i >> 16) & 0xff), (byte) ((i >> 8) & 0xff),
+				(byte) (i & 0xff) };
+		Inet4Address addr;
+		try {
+			addr = (Inet4Address) InetAddress.getByAddress(b);
+		} catch (UnknownHostException e) {
+			return null;
+		}
+
+		return addr;
+	}
+
+	/**
+	 * returns actions that forward the packets arriving into the sw to the
+	 * nextHost this method this action should be set to the switch attached to
+	 * the host uses the routing service from constructor
+	 * 
+	 * 
+	 * @param host
+	 * @param nextHost
+	 * @return
+	 */
+	private NodeConnector outputToDst(Node sw, HostNodeConnector nextHost) {
+		NodeConnector result = null;
+		Path route = routing.getRoute(sw, nextHost.getnodeconnectorNode());
+		if (route == null) { // destination host is attached to switch
+			result = nextHost.getnodeConnector();
+		} else { // forward to destination host
+			result = route.getEdges().get(0).getTailNodeConnector();
+		}
+		return result;
+	}
+
+	private Flow generateRouteToHostFlow(HostNodeConnector host,
+			Node incoming_node) {
+		short priority = 1;
+		List<Action> actions = new ArrayList<Action>();
+		Action output = new Output(outputToDst(incoming_node, host));
+		actions.add(output);
+		Match match = new Match();
+		match.setField(MatchType.DL_TYPE, (short) 0x0800);
+		match.setField(MatchType.NW_DST, host.getNetworkAddress());
+		Flow flow = new Flow(match, actions);
+		flow.setPriority(priority);
+		return flow;
 	}
 
 	private void floodPacket(RawPacket inPkt) {
@@ -190,7 +273,13 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 
 		for (NodeConnector p : nodeConnectors) {
 			if (!p.equals(incoming_connector)) {
-				sendPacket(inPkt, p);
+				try {
+					RawPacket destPkt = new RawPacket(inPkt);
+					destPkt.setOutgoingNodeConnector(p);
+					this.dataPacketService.transmitDataPacket(destPkt);
+				} catch (ConstructionException e2) {
+					continue;
+				}
 			}
 		}
 	}
@@ -207,65 +296,58 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 		NodeConnector incoming_connector = inPkt.getIncomingNodeConnector();
 		Node incoming_node = incoming_connector.getNode();
 
-		if (formattedPak instanceof Ethernet) {
-			byte[] srcMAC = ((Ethernet) formattedPak).getSourceMACAddress();
-			byte[] dstMAC = ((Ethernet) formattedPak)
-					.getDestinationMACAddress();
+		if (!(formattedPak instanceof Ethernet)) {
+			return PacketResult.IGNORED;
+		}
+		Ethernet l2Pkt = (Ethernet) formattedPak;
+		Packet l3Pkt = l2Pkt.getPayload();
+		if (!(l3Pkt instanceof IPv4)) {
+			floodPacket(inPkt);
+			return PacketResult.IGNORED;
+		}
+		IPv4 ipPacket = (IPv4) l3Pkt;
+		Inet4Address ipDest = intToInetAddress(ipPacket.getDestinationAddress());
+		logger.debug("got ip packet to: " + ipDest);
+		HostNodeConnector dstHost;
+		try {
+			dstHost = hostTracker.discoverHost(ipDest).get();
 
-			// Hub implementation
-			if (function.equals("hub")) {
+			if (dstHost == null) {
 				floodPacket(inPkt);
-				return PacketResult.CONSUME;
+				return PacketResult.KEEP_PROCESSING;
 			}
 
-			// Switch
-			else {
-				long srcMAC_val = BitBufferHelper.toNumber(srcMAC);
-				long dstMAC_val = BitBufferHelper.toNumber(dstMAC);
+			Flow flow = generateRouteToHostFlow(dstHost, incoming_node);
+			if (!programFlow(incoming_node, dstHost, flow))
+				return PacketResult.IGNORED;
+			sendPacket(inPkt, outputToDst(incoming_node, dstHost));
 
-				Match match = new Match();
-				match.setField(new MatchField(MatchType.IN_PORT,
-						incoming_connector));
-				match.setField(new MatchField(MatchType.DL_DST, dstMAC.clone()));
-
-				// Set up the mapping: switch -> src MAC address -> incoming
-				// port
-				if (this.mac_to_port_per_switch.get(incoming_node) == null) {
-					this.mac_to_port_per_switch.put(incoming_node,
-							new HashMap<Long, NodeConnector>());
-				}
-				this.mac_to_port_per_switch.get(incoming_node).put(srcMAC_val,
-						incoming_connector);
-
-				NodeConnector dst_connector = this.mac_to_port_per_switch.get(
-						incoming_node).get(dstMAC_val);
-
-				// Do I know the destination MAC?
-				if (dst_connector != null) {
-
-					List<Action> actions = new ArrayList<Action>();
-					actions.add(new Output(dst_connector));
-
-					Flow f = new Flow(match, actions);
-					f.setPriority((short) 1);
-
-					// Modify the flow on the network node
-					Status status = programmer.addFlow(incoming_node, f);
-					if (!status.isSuccess()) {
-						logger.warn(
-								"SDN Plugin failed to program the flow: {}. The failure is: {}",
-								f, status.getDescription());
-						return PacketResult.IGNORED;
-					}
-					logger.debug("Installed flow {} in node {}", f,
-							incoming_node);
-					sendPacket(inPkt, dst_connector);
-					return PacketResult.CONSUME;
-				} else
-					floodPacket(inPkt);
-			}
+			return PacketResult.CONSUME;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return PacketResult.IGNORED;
+	}
+
+	private boolean programFlow(Node incoming_node, HostNodeConnector dstHost,
+			Flow flow) {
+		Status status = programmer.addFlow(incoming_node, flow);
+		if (!status.isSuccess()) {
+			logger.warn(
+					"SDN Plugin failed to program the flow: {}. The failure is: {}",
+					flow, status.getDescription());
+			return false;
+		}
+		logger.info("Installed flow {} in node {}", flow, incoming_node);
+		_allFlows.put(flow, incoming_node);
+		if (!_hostFlows.containsKey(dstHost))
+			_hostFlows.put(dstHost, new LinkedList<Flow>());
+		_hostFlows.get(dstHost).add(flow);
+		return true;
 	}
 
 	private void sendPacket(RawPacket inPkt, NodeConnector dst_connector) {
@@ -280,4 +362,29 @@ public class TutorialL2Forwarding implements IListenDataPacket {
 		}
 
 	}
+
+	@Override
+	public void notifyHTClient(HostNodeConnector host) {
+		logger.info("new host added: " + host);
+
+	}
+
+	@Override
+	public void notifyHTClientHostRemoved(HostNodeConnector host) {
+		logger.info("host removed: " + host.getNetworkAddressAsString());
+		removeHostFlows(host);
+	}
+
+	private void removeHostFlows(HostNodeConnector host) {
+		if (!_hostFlows.containsKey(host)) {
+			logger.warn("removed host has no flows, shouldnt happen");
+			return;
+		}
+		logger.info("removing host flows: " + host.getNetworkAddressAsString());
+		for (Flow flow : _hostFlows.get(host)) {
+			removeFlow(flow);
+		}
+		_hostFlows.remove(host);
+	}
+
 }
